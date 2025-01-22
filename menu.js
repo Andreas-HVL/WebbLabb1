91889996f696a6fadae86a2d72a32e8575c200d7
Hijaker <30287680+Hijaker@users.noreply.github.com>
2025-01-22 14:19:14 +0100

    products added, need to add images

---
+++ menu.js
@@ -0,0 +1,112 @@
+const menu = [
+    // Pizzas
+    {
+      name: "Margherita",
+      ingredients: ["Tomato sauce", "Mozzarella", "Basil"],
+      price: 120, 
+      category: "Pizza",
+      img: "" // Add image link here
+    },
+    {
+      name: "Pepperoni",
+      ingredients: ["Tomato sauce", "Mozzarella", "Pepperoni"],
+      price: 135, 
+      category: "Pizza",
+      img: "" // Add image link here
+    },
+    {
+      name: "Hawaiian",
+      ingredients: ["Tomato sauce", "Mozzarella", "Ham", "Pineapple"],
+      price: 145, 
+      category: "Pizza",
+      img: "" // Add image link here
+    },
+    {
+      name: "Veggie",
+      ingredients: ["Tomato sauce", "Mozzarella", "Bell peppers", "Olives", "Onions"],
+      price: 125, 
+      category: "Pizza",
+      img: "" // Add image link here
+    },
+    {
+      name: "Meat Lover's",
+      ingredients: ["Tomato sauce", "Mozzarella", "Ham", "Sausage", "Bacon"],
+      price: 150, 
+      category: "Pizza",
+      img: "" // Add image link here
+    },
+    {
+      name: "Four Cheese",
+      ingredients: ["Tomato sauce", "Mozzarella", "Parmesan", "Gorgonzola", "Ricotta"],
+      price: 140, 
+      category: "Pizza",
+      img: "" // Add image link here
+    },
+    {
+      name: "BBQ Chicken",
+      ingredients: ["BBQ sauce", "Mozzarella", "Chicken", "Onions", "Coriander"],
+      price: 145, 
+      category: "Pizza",
+      img: "" // Add image link here
+    },
+    {
+      name: "Seafood",
+      ingredients: ["Tomato sauce", "Mozzarella", "Shrimp", "Mussels", "Garlic"],
+      price: 155, 
+      category: "Pizza",
+      img: "" // Add image link here
+    },
+    {
+      name: "Spicy Salami",
+      ingredients: ["Tomato sauce", "Mozzarella", "Spicy salami", "Chili flakes"],
+      price: 140, 
+      category: "Pizza",
+      img: "" // Add image link here
+    },
+    {
+      name: "Truffle Mushroom",
+      ingredients: ["Cream sauce", "Mozzarella", "Truffle oil", "Mushrooms"],
+      price: 160, 
+      category: "Pizza",
+      img: "" // Add image link here
+    },
+    
+    // Non-pizza entries
+    {
+      name: "Caesar Salad",
+      ingredients: ["Lettuce", "Parmesan", "Croutons", "Caesar dressing", "Chicken"],
+      price: 110, 
+      category: "Non-pizza",
+      img: "" // Add image link here
+    },
+    {
+      name: "Garlic Bread",
+      ingredients: ["Bread", "Garlic butter", "Parsley"],
+      price: 50, 
+      category: "Non-pizza",
+      img: "" // Add image link here
+    },
+    {
+      name: "Spaghetti Carbonara",
+      ingredients: ["Spaghetti", "Eggs", "Pancetta", "Parmesan", "Black pepper"],
+      price: 130, 
+      category: "Non-pizza",
+      img: "" // Add image link here
+    },
+    {
+      name: "Tiramisu",
+      ingredients: ["Mascarpone", "Espresso", "Ladyfingers", "Cocoa powder"],
+      price: 85, 
+      category: "Dessert",
+      img: "" // Add image link here
+    },
+    {
+      name: "Minestrone Soup",
+      ingredients: ["Tomatoes", "Beans", "Pasta", "Vegetables"],
+      price: 95, 
+      category: "Non-pizza",
+      img: "" // Add image link here
+    }
+  ];
+  
+export default menu;
