f75262601d2f0bd8b9fb45f59461eeec5d3437a6
Andreas-HVL <andreas.hoft.vigh.larsen@iths.se>
2025-01-22 22:06:35 +0100

    updated menu and index, added cart

--- menu.js
+++ menu.js
@@ -5,70 +5,70 @@ const menu = [
       ingredients: ["Tomato sauce", "Mozzarella", "Basil"],
       price: 120, 
       category: "Pizza",
-      img: "" // Add image link here
+      img: "https://images.arla.com/recordid/17ADC8DB-DBC5-4DFA-AA85A07EEF36E4E7/pizza-margherita.jpg?format=jpg&width=375&height=265&mode=crop&crop=(187,0,-91,0)" 
     },
     {
       name: "Pepperoni",
       ingredients: ["Tomato sauce", "Mozzarella", "Pepperoni"],
       price: 135, 
       category: "Pizza",
-      img: "" // Add image link here
+      img: "https://atsloanestable.com/wp-content/uploads/2023/06/new-york-style-pizza2-500x500.jpg" 
     },
     {
       name: "Hawaiian",
       ingredients: ["Tomato sauce", "Mozzarella", "Ham", "Pineapple"],
       price: 145, 
       category: "Pizza",
-      img: "" // Add image link here
+      img: "https://hips.hearstapps.com/hmg-prod/images/hawaiian-pizza-lead-65f4641d61062.jpg" 
     },
     {
       name: "Veggie",
-      ingredients: ["Tomato sauce", "Mozzarella", "Bell peppers", "Olives", "Onions"],
+      ingredients: ["Tomato sauce", "Tomatoes", "Mozzarella", "Bell peppers", "Olives", "Onions"],
       price: 125, 
       category: "Pizza",
-      img: "" // Add image link here
+      img: "https://popmenucloud.com/cdn-cgi/image/width%3D1200%2Cheight%3D1200%2Cfit%3Dscale-down%2Cformat%3Dauto%2Cquality%3D60/khmjfnga/3b6c4fe5-efb9-4b43-9d61-4cab66781c4f.jpg" 
     },
     {
       name: "Meat Lover's",
-      ingredients: ["Tomato sauce", "Mozzarella", "Ham", "Sausage", "Bacon"],
+      ingredients: ["Tomato sauce", "Mozzarella", "Ham", "pepperoni", "Minced Meat"],
       price: 150, 
       category: "Pizza",
-      img: "" // Add image link here
+      img: "https://img.freepik.com/premium-photo/top-view-meat-lovers-pizza-with-assortment-pepperoni-sausage-bacon-ham-rich-tomato-sauce-base_1354780-2974.jpg" 
     },
     {
       name: "Four Cheese",
       ingredients: ["Tomato sauce", "Mozzarella", "Parmesan", "Gorgonzola", "Ricotta"],
       price: 140, 
       category: "Pizza",
-      img: "" // Add image link here
+      img: "https://kitchenatics.com/wp-content/uploads/2020/09/Cheese-pizza-1.jpg" 
     },
     {
       name: "BBQ Chicken",
       ingredients: ["BBQ sauce", "Mozzarella", "Chicken", "Onions", "Coriander"],
       price: 145, 
       category: "Pizza",
-      img: "" // Add image link here
+      img: "https://therecipecritic.com/wp-content/uploads/2022/04/bbqchickenpizza-1-2.jpg" 
     },
     {
       name: "Seafood",
       ingredients: ["Tomato sauce", "Mozzarella", "Shrimp", "Mussels", "Garlic"],
       price: 155, 
       category: "Pizza",
-      img: "" // Add image link here
+      img: "https://palatablepastime.com/wp-content/uploads/2022/12/greek-style-seafood-pizza-og.jpg" 
     },
     {
       name: "Spicy Salami",
       ingredients: ["Tomato sauce", "Mozzarella", "Spicy salami", "Chili flakes"],
       price: 140, 
       category: "Pizza",
-      img: "" // Add image link here
+      img: "https://therecipecritic.com/wp-content/uploads/2022/12/salami_pizza-1.jpg" 
     },
     {
       name: "Truffle Mushroom",
       ingredients: ["Cream sauce", "Mozzarella", "Truffle oil", "Mushrooms"],
       price: 160, 
       category: "Pizza",
-      img: "" // Add image link here
+      img: "https://images.squarespace-cdn.com/content/v1/55be995de4b071c106b3b4c0/1607620549016-GKSGISFJAM5R01SSUT9T/Truffle+Mushroom+and+Ricotta+Pizza+-+Eat+Cho+Food+Blog" 
     },
     
     // Non-pizza entries
@@ -77,35 +77,28 @@ const menu = [
       ingredients: ["Lettuce", "Parmesan", "Croutons", "Caesar dressing", "Chicken"],
       price: 110, 
       category: "Non-pizza",
-      img: "" // Add image link here
+      img: "https://feelgoodfoodie.net/wp-content/uploads/2020/04/Caesar-Salad-TIMG.jpg" 
     },
     {
       name: "Garlic Bread",
       ingredients: ["Bread", "Garlic butter", "Parsley"],
       price: 50, 
       category: "Non-pizza",
-      img: "" // Add image link here
+      img: "https://www.allrecipes.com/thmb/ymrjQ3GFq_Fc7Fu2yfvIj108tcM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/21080-great-garlic-bread-DDMFS-4x3-e1c7b5c79fde4d629a9b7bce6c0702ed.jpg" 
     },
     {
       name: "Spaghetti Carbonara",
       ingredients: ["Spaghetti", "Eggs", "Pancetta", "Parmesan", "Black pepper"],
       price: 130, 
       category: "Non-pizza",
-      img: "" // Add image link here
-    },
-    {
-      name: "Tiramisu",
-      ingredients: ["Mascarpone", "Espresso", "Ladyfingers", "Cocoa powder"],
-      price: 85, 
-      category: "Dessert",
-      img: "" // Add image link here
+      img: "https://www.allrecipes.com/thmb/Vg2cRidr2zcYhWGvPD8M18xM_WY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/11973-spaghetti-carbonara-ii-DDMFS-4x3-6edea51e421e4457ac0c3269f3be5157.jpg" 
     },
     {
       name: "Minestrone Soup",
       ingredients: ["Tomatoes", "Beans", "Pasta", "Vegetables"],
       price: 95, 
       category: "Non-pizza",
-      img: "" // Add image link here
+      img: "https://cdn.loveandlemons.com/wp-content/uploads/2021/11/minestrone-soup-500x500.jpg" 
     }
   ];
   
